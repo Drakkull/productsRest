18dc350f94fefa3f37cbbc212824d4822013ed88
Leonardo DiAbreu <drakkull007@hotmail.com>
2024-03-14 10:28:02 -0300

    Added few classes and Post method is created into the Product Controller

---
+++ src/main/java/com/products/Produtos/dtos/ProductRecordDto.java
@@ -0,0 +1,13 @@
+package com.products.Produtos.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+
+public record ProductRecordDto(@NotBlank String name,@NotNull BigDecimal value) {
+
+
+
+
+}
